06abc2f439de8c9fa01de413b089057ebedfe89a
Nemi Sanghvi <nemisanghvi009@gmail.com>
2025-02-12 16:14:43 +0530

    Resume Update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Page from './Components/Page.jsx'
-import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
